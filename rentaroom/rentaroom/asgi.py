ef142abafa7a792677b7ac532566ba98ae24b16b
Piyush Jakhar <piyushjakhar08@gmail.com>
2021-09-12 21:06:24 -0700

    django backend set

--- rentaroom/rentaroom/asgi.py
+++ rentaroom/rentaroom/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for room project.
+ASGI config for rentaroom project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'room.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rentaroom.settings')
 
 application = get_asgi_application()
